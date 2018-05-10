f2f34dcb776afb66d271554f9ed85813fe153e97
Eamonn Gaynor <gaynor5555@gmail.com>
2018-05-10 11:45:41 +0100

    Lab 5 added
    Doxyfile created on Make, Producer/Consumer program

---
+++ Lab5/Semaphore.h
@@ -0,0 +1,44 @@
+/*! 
+
+
+Author: Eamonn Gaynor
+
+Date: 08/10/2018
+
+File: Semaphore.h
+
+Brief: Uses C++11 features such as mutex and condition variables to implement Semaphore
+
+Bugs: N/A
+
+Licence: GNU (General Public License)
+
+
+*/
+
+
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+/*! \class Semaphore
+    \brief A Semaphore Implementation
+
+   Uses C++11 features such as mutex and condition variables to implement Semaphore
+
+*/
+class Semaphore
+{
+private:
+    unsigned int m_uiCount; /*!< Holds the Semaphore count */
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+
+public:
+    Semaphore(unsigned int uiCount=0)
+          : m_uiCount(uiCount) { };
+    void Wait();
+    template< typename R,typename P >
+    bool Wait(const std::chrono::duration<R,P>& crRelTime);
+    void Signal();
+
+};
